15eb678c193cf5cf549740294aa2a590ceada1b0
Justin Ross <jross@apache.org>
2020-03-04 06:26:48 -0500

    WIP

--- input/_transom/config.py
+++ input/_transom/config.py
@@ -9,8 +9,8 @@ def _render_cell(row_index, column_index, cell):
 process_table = html_table_csv("data/processes.csv", cell_render_fn=_render_cell)
 
 _app_data = [
-    ["Source code repo", "<a href=\"https://github.com/companyco\">https://github.com/companyco</a>"],
-    ["Container image repo", "<a href=\"https://quay.io/companyco\">https://quay.io/companyco</a>"],
+    ["Source code", "<a href=\"https://github.com/companyco\">https://github.com/companyco</a>"],
+    ["Container images", "<a href=\"https://quay.io/companyco\">https://quay.io/companyco</a>"],
 ]
 
-app_properties = html_table(_app_data, column_headings=False, row_headings=True)
+app_properties = html_table(_app_data, column_headings=False, row_headings=True, class_="properties")
